7f64e2ee76a137c39c27ae559d4468f2e0d11503
Lekhan2004 <113852037+Lekhan2004@users.noreply.github.com>
2023-05-12 14:05:04 -0700

    Update TOH without Rec.cpp

--- TOH without Rec.cpp
+++ TOH without Rec.cpp
@@ -4,12 +4,12 @@
     char S, D, Au;
 } ;
 void TOH(int n, char S, char D, char Au) {
-    Tower stack[n];
+   struct Tower stack[n];
     int top = -1;
-    Tower T1 = { n, S, D, Au };
+      struct Tower T1 = { n, S, D, Au };
     stack[++top] = T1;
     while (top >= 0) {
-        Tower t = stack[top--];
+         struct  Tower t = stack[top--];
         n = t.n;
         S = t.S;
         D = t.D;
@@ -17,11 +17,11 @@ void TOH(int n, char S, char D, char Au) {
         if (n == 1) {
             printf("Move disk 1 from rod %c to rod %c\n", S, D);
         } else {
-            Tower T3 = { n - 1, Au, D, S };
+             struct  Tower T3 = { n - 1, Au, D, S };
             stack[++top] = T3;
-            Tower T2 = { 1, S, D, Au };
+              struct Tower T2 = { 1, S, D, Au };
             stack[++top] = T2;
-            Tower T4 = { n - 1, S, Au, D };
+              struct Tower T4 = { n - 1, S, Au, D };
             stack[++top] = T4;
         } } 
 }
