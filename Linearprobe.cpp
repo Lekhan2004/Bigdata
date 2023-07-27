a7d4e15076c494c8b723bf80277752f68246afb7
Lekhan2004 <113852037+Lekhan2004@users.noreply.github.com>
2023-07-26 22:47:28 -0700

    Update Linearprobe.cpp

--- Linearprobe.cpp
+++ Linearprobe.cpp
@@ -6,40 +6,58 @@ int h1(int ele){
 	return ele%n;
 }
 void insert(int ele,int hashtab[]){
-	int x;
-	x=h1(ele);
-	if(hashtab[x]==-1){
-		hashtab[x]=ele;
-	}else{
-		int i=0;
-		while(1){
-			x=h1(x)+i;
-			if(hashtab[x]==-1){
-					hashtab[x]=ele;
-					break;
-			}
-	}
+	int x,i,hkey;
+	x=ele%n;
+	for(i=0;i<n;i++)  
+   {  
+    hkey=(x+i)%n;  
+    if(hashtab[hkey] == -1)  
+    {  
+     hashtab[hkey]=ele;  
+      break;  
+    }  
+   }  
+   if(i == n)  
+    printf("\nelement cannot be inserted\n");  
 }
+void search(int ele,int hashtab[]){
+	int i,hkey;
+	for(i=0;i<n;i++)  
+   {  
+    hkey=(ele%n+i)%n;  
+    if(hashtab[hkey] == -1)  
+    {  
+	printf("Not Found\n"); 
+    break;  
+    }
+	else if(hashtab[hkey]==ele){
+		printf("Found\n");
+		break;
+	}  
+   }
+   if(i==n)  printf("Not Found\n"); 
 }
 void display(int hashtab[]){
 	for(int i=0;i<n;i++)
 	printf("At Hashtab[%d] : %d\n",i,hashtab[i]);
 }
 int main(){
-
 	printf("Enter size of the Hashtable\n");
 	scanf("%d",&n);
 	int hashtab[n],ele,choice;
 	for(int i=0;i<n;i++)hashtab[i]=-1;
 	do{
-		printf("Enter the operation you want to perform\n1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+		printf("Enter the operation you want to perform\n1.Insert\n2.Search\n3.Display\n4.Exit\n");
 		scanf("%d",&choice);
 		switch(choice){
 			case 1:printf("Enter the key you want to insert\n");
 				   scanf("%d",&ele);
 				   insert(ele,hashtab);
 				   break;
-			case 2://del();
+			case 2:printf("Enter the key you want to search\n");
+				   scanf("%d",&ele);
+				   search(ele,hashtab);
+				   break;
 			case 3:display(hashtab);break;
 			case 4:exit(0);	   
 		}
